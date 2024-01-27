4c5a9fda87c0dbe2e0fd6309beb6ef426225125b
mrroot <baisalkalykov69@gmail.com>
2024-01-27 16:49:15 +0600

    TodoItem

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoItem from './conponents/home/TodoItem'
+function App() {
+  return (
+    <div>
+      <TodoItem/>
+    </div>
+  )
+}
+
+export default App
+
+
